4102b70695c0701823d74c2633ea5a61a89071b4
Gaute Tessand Baalsrud <gaute.tb@gmail.com>
2018-02-12 12:56:33 +0100

    testtest

--- Semesteroppgave/src/testklasse.java
+++ Semesteroppgave/src/testklasse.java
@@ -3,5 +3,9 @@ public class testklasse {
     public static void main(String[] args) {
 
         System.out.println("heiheiehihi");
+
+        int x = 1;
+
+        System.out.println(x);
     }
 }
