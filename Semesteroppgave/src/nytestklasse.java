74108c42039705008207954b958146a3c6ac59a7
Gaute Tessand Baalsrud <gaute.tb@gmail.com>
2018-03-01 11:23:30 +0100

    jobber med filbehandling og lagring

--- Semesteroppgave/src/nytestklasse.java
+++ Semesteroppgave/src/nytestklasse.java
@@ -1,7 +1,18 @@
+import java.io.*;
+
 public class nytestklasse {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         System.out.println("Dette er feature-gaute");
+        File loadFile = new File("obj.txt");
+
+        FileInputStream fis = new FileInputStream(loadFile);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        
+
+
     }
+
+
 }
