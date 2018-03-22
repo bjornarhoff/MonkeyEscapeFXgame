1028ec12b4fe1eb6e204a56ae649318b9c5cbdaa
Gaute Tessand Baalsrud <gaute.tb@gmail.com>
2018-03-22 18:15:40 +0100

    Lagt til javadoc og ryddet opp litt i klassene. Jeg har enda ikke klart å få mer kode ut av main-klassen.

--- Semesteroppgave/src/spillet/ape.java
+++ Semesteroppgave/src/spillet/ape.java
@@ -1,20 +1,44 @@
 package spillet;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * Dette er class for avataren til spilleren.
+ *
+ * @Gaute, @Eirik og @Bjørnar
+ */
 
 public class ape extends Avatar {
 
-    private double positionX;
-    private double positionY;
-
-    public ape () {
-        positionX = 0;
-        positionY = 0;
+    /**
+     * Constructor for ape, denne overrider konstruktøren til avatar.
+     * @param filename
+     * @param x
+     * @param y
+     */
+    public ape (String filename, double x, double y) {
+        super(filename, x, y);
+        Image bilde = new Image(filename);
+        setImage(bilde);
+        setPositionY(y);
+        setPositionX(x);
     }
 
-    @Override
-    public void render(GraphicsContext gc, double w, double h)
-    {
-        gc.drawImage(getImage(), getPositionX(), getPositionY(), w, h);
+    /**
+     * Dette er en metode for å bevege spilleren basert på string-nøkkelord som kommer inn.
+     * Den brukes i launcherklassen for å oppdatere posisjonen til spilleren basert på tastetrykk med piltastene
+     * @param retning
+     */
+    public void beveg(String retning) {
+        if (retning.equals("Opp")) {
+            setPositionY(getPositionY() - 10);
+        } else if (retning.equals("Ned")) {
+            setPositionY(getPositionY() + 10);
+        } else if (retning.equals("Venstre")) {
+            setPositionX(getPositionX() - 10);
+        } else if (retning.equals("Høyre")) {
+            setPositionX(getPositionX() + 10);
+        }
     }
 }
--- Semesteroppgave/src/spillet/main.java
+++ Semesteroppgave/src/spillet/main.java
@@ -13,17 +13,20 @@ import javafx.stage.*;
 import javafx.scene.input.KeyEvent;
 import javafx.event.EventHandler;
 
-
+/**
+ * Launcherklasse som inneholder main-metoden og start-metoden for å danne scenen, samt animere innholdet.
+ *
+ * @Gaute, @Eirik og @Bjørnar
+ */
 public class main extends Application {
 
     private GraphicsContext grafikk;
     private Canvas lerret;
     private Pane root;
     private Scene vinduInnhold;
-    private Avatar ape;
+    private ape player;
     private Image bakgrunn;
-    private Image tre1;
-    private Image tre2;
+    private Image tre1, tre2, tre3, tre4, tre5, tre6;
     private double apebredde = 100;
     private double apehøyde = 100;
     private AnimationTimer timer;
@@ -33,6 +36,11 @@ public class main extends Application {
         launch(args);
     }
 
+    /**
+     * Metoden overrider start-metoden i Application og starter vinduet og legger til innholdet.
+     * @param vindu
+     * @throws Exception
+     */
     @Override
     public void start(Stage vindu) throws Exception {
         vindu.setTitle("Prøvespill");
@@ -44,22 +52,26 @@ public class main extends Application {
         vindu.show();
     }
 
+    /**
+     * Denne metoden danner layoyt-pane som man legger canvas og dermed animasjonene på.
+     * @return root
+     */
     private Parent lagVerden() {
         root = new Pane();
         lerret = new Canvas(600, 600);
         grafikk = lerret.getGraphicsContext2D();
 
-
-        ape = new Avatar();
+        player = new ape("spillet/ape.png", 150, 150);
         bakgrunn = new Image("spillet/bakgrunn.png");
         tre1 = new Image("spillet/tre.png");
         tre2 = new Image("spillet/tre.png");
+        tre3 = new Image("spillet/tre.png");
+        tre4 = new Image("spillet/tre.png");
+        tre5 = new Image("spillet/tre.png");
+        tre6 = new Image("spillet/tre.png");
         renderVerden();
 
-        ape.setPositionX(150);
-        ape.setPositionY(150);
-        ape.setImage("spillet/ape.png");
-        ape.render(grafikk, apebredde, apehøyde);
+        player.render(grafikk, apebredde, apehøyde);
 
         root.getChildren().add(lerret);
 
@@ -74,25 +86,35 @@ public class main extends Application {
         return root;
     }
 
+    /**
+     * Metode som fjerner og reanimerer innholdet i scenen.
+     */
     public void renderVerden() {
-        grafikk.clearRect(0, 0, 30, 30);
+        grafikk.clearRect(0, 0, 600, 600);
         grafikk.drawImage(bakgrunn, 0, 0, 600, 600);
-        grafikk.drawImage(tre1, 400, 50, 50, 300);
-        grafikk.drawImage(tre2, 50, 50, 350,50);
-        ape.render(grafikk, apebredde, apehøyde);
+        grafikk.drawImage(tre1, 60, 0, 540, 10);
+        grafikk.drawImage(tre2, 590, 10, 10,590);
+        grafikk.drawImage(tre3, 0, 590, 590, 10);
+        grafikk.drawImage(tre4, 0, 0, 10,590);
+        grafikk.drawImage(tre5, 60, 0, 540, 10);
+        grafikk.drawImage(tre6, 590, 10, 10,590);
+        player.render(grafikk, apebredde, apehøyde);
     }
 
+    /**
+     * Metode som oppdaterer posisjonen til spilleren i x- og y-retning basert på tastetrykk med piltastene.
+     */
     public void moveApe() {
         vinduInnhold.setOnKeyPressed(new EventHandler<KeyEvent>() {
             public void handle(KeyEvent e) {
                 if (e.getCode() == KeyCode.UP) {
-                    ape.setPositionY(ape.getPositionY() - 10);
+                    player.beveg("Opp");
                 } else if (e.getCode() == KeyCode.DOWN) {
-                    ape.setPositionY(ape.getPositionY() + 10);
+                    player.beveg("Ned");
                 } else if (e.getCode() == KeyCode.LEFT) {
-                    ape.setPositionX(ape.getPositionX() - 10);
+                    player.beveg("Venstre");
                 } else if (e.getCode() == KeyCode.RIGHT) {
-                    ape.setPositionX(ape.getPositionX() + 10);
+                    player.beveg("Høyre");
                 }
             }
         });
